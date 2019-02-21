69951eff9cf7ab1f7dfa1f450c9bccca866e7852
Rob Revels <rob.revels@cloudability.com>
2019-02-21 06:38:29 -0800

    get working on lambda

--- milkrun.go
+++ milkrun.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -11,6 +12,9 @@ import (
 	"net/url"
 	"os"
 
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
 	"golang.org/x/net/publicsuffix"
 )
 
@@ -22,15 +26,38 @@ const (
 	checkoutsUrl    = apiUrl + "/checkouts"
 )
 
-func main() {
-	email := os.Getenv("MILKRUN_EMAIL")
-	password := os.Getenv("MILKRUN_PASSWORD")
+var encrypted string = os.Getenv("MILKRUN_PASSWORD")
+var decrypted string
 
-	fmt.Printf("email %s password %s", email, password)
-	return
+func init() {
+	kmsClient := kms.New(session.New())
+	decodedBytes, err := base64.StdEncoding.DecodeString(encrypted)
+	if err != nil {
+		panic(err)
+	}
+	input := &kms.DecryptInput{
+		CiphertextBlob: decodedBytes,
+	}
+	response, err := kmsClient.Decrypt(input)
+	if err != nil {
+		panic(err)
+	}
+	// Plaintext is a byte array, so convert to string
+	decrypted = string(response.Plaintext[:])
+}
+func main() {
+	lambda.Start(HandleRequest)
+}
+
+func HandleRequest() (string, error) {
+	log.Print("Starting Milkrun")
+	email := os.Getenv("MILKRUN_EMAIL")
+	password := decrypted
+
 	resp, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
 	if err != nil {
 		log.Fatal(err)
+		return "", err
 	}
 
 	client := &http.Client{
@@ -38,34 +65,38 @@ func main() {
 	}
 
 	//login
-
+	log.Print("Logging In")
 	_, err = client.PostForm(authUrl,
 		url.Values{"email": {email}, "password": {password}})
 	if err != nil {
 		log.Fatal(err)
-		return
+		return "", err
 	}
+
+	log.Print("Logged In")
 
 	//order milk
 	orderJson := []byte(`{"product_id":1837,"add_quantity":6}`)
 	_, err = client.Post(cartContentsUrl, "application/json;charset=utf-8", bytes.NewBuffer(orderJson))
 	if err != nil {
 		log.Fatal(err)
-		return
+		return "", err
 	}
 
+	log.Print("Ordered Milk")
 	//get checkout
 
+	log.Print("Checking out")
 	checkoutResponse, err := client.Post(checkoutsUrl, "application/json;charset=utf-8", nil)
 	if err != nil {
 		log.Fatal(err)
-		return
+		return "", err
 	}
 
 	checkoutJson, err := ioutil.ReadAll(checkoutResponse.Body)
 	if err != nil {
 		log.Fatal(err)
-		return
+		return "", err
 	}
 
 	type Checkout struct {
@@ -77,25 +108,29 @@ func main() {
 
 	//finalize checkout
 
+	log.Print("Finalizing Checkout")
 	finalizePayload := fmt.Sprintf(`{"id": %d}`, checkout.ID)
 	finalizeUrl := fmt.Sprintf(apiUrl+"/checkouts/%d/order", checkout.ID)
 	finalizeCheckoutResponse, err := client.Post(finalizeUrl, "application/json;charset=utf-8", bytes.NewBuffer([]byte(finalizePayload)))
 	if err != nil {
 		log.Fatal(err)
-		return
+		return "", err
 	}
 
 	finalizeJson, err := ioutil.ReadAll(finalizeCheckoutResponse.Body)
 	if err != nil {
 		log.Fatal(err)
-		return
+		return "", err
 	}
-	log.Println("JSON %s", finalizeJson)
+	log.Printf("Finalized! JSON %s", finalizeJson)
 	//logout
 
 	req, _ := http.NewRequest("DELETE", authUrl, nil)
 	_, err = http.DefaultClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
+		return "", err
 	}
+	log.Print("Logged out")
+	return "success", nil
 }
